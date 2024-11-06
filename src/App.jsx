8e39d9671f79df0be86b28043c0ffcca13ea497d
magicenvision <rjrose2003@gmail.com>
2024-11-05 20:40:24 -0500

    second NavBar Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import Navbar1 from './components/Navbar1'
+import NavBar2 from './components/NavBar2'
 
 
 const App = () => {
-
+  
   return (
     <>
-    <Navbar1/>
-  
+       <style>
+        {`
+          html, body {
+            height: 100%;
+            margin: 0;
+            background-color: black;
+            color: white;
+          }
+        `}
+      </style>
+      < div className='bg-black h-full'>
+        <Navbar1 />
+      </div>
+      <NavBar2 />
+
     </>
 
   )
+  
 }
 
 export default App
--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -2,19 +2,15 @@ import React from 'react'
 
 const Navbar1 = () => {
     return (
-        <div className='w-full border h-20 bg-red-50 border-b-slate-950 flex items-center justify-between '>
+        <div className='w-full h-20 bg-red-50 border-b-8 border-b-slate-950 flex items-center justify-between '>
             <h1 className='text-4xl font-bold font-serif px-2 bg-red-400 text-transparent bg-clip-text inline-block'><a href="#">Example</a>
                 <span className='text-black'>+</span>
             </h1>
            <div className="flex px-2 font-semibold items-center text-center">
-           <a className="p-1 px-2 hover:border-red-400 border bg-red-300" href="#">Home</a>
-            <a className="p-1 px-2 hover:border-red-400 border bg-red-300">Pricing</a>
-            <a className="p-1 px-2 hover:border-red-400 border bg-red-300" href="#">Testimonials</a>
+           <a className="p-1 px-2 hover:border-red-400 border rounded-md bg-red-300" href="#">Home</a>
+            <a className="p-1 px-2 hover:border-red-400 border  rounded-md bg-red-300">Pricing</a>
+            <a className="p-1 px-2 hover:border-red-400 border  rounded-md  bg-red-300" href="#">Testimonials</a>
            </div>
-
-
-
-
         </div>
     )
 }
