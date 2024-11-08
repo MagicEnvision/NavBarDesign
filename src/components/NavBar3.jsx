511fe094d198153c620249164248c836aeae26d7
magicenvision <rjrose2003@gmail.com>
2024-11-07 19:20:36 -0500

    Working responsive BBC 95% Needs dropdown menu

--- src/components/NavBar3.jsx
+++ src/components/NavBar3.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NavBar3 = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
     return (
         <nav>
             <div className="bg-slate-800 px-5 sm:px-[5rem]  w-full h-12 flex items-center justify-between mr-10">
@@ -22,24 +23,28 @@ const NavBar3 = () => {
                         <li className='hover:bg-sky-500 px-3 h-full flex items-center'>World-Wide</li>
                     </ul>
                 </div>
+                
                 <div className=" hidden md:flex gap-3 items-center">
                     <i className='bx bx-search text-white font-semibold'></i>
                     <input type="text" placeholder='Search' className=' rounded-lg px-2' />
                 </div>
+                <i className='xl:hidden text-5xl bx bx-menu text-white' onClick={() => setMenuOpen(!menuOpen)}> 
+                </i>
+              
+
+            
             </div>
+            {/* <ul className={`left-0  flex flex-col w-full justify-center xl:hidden items-center ${menuOpen ? "opacity-100" : "opacity-0"}`}>
+                    <li className='hover:bg-sky-500 px-3 w-full flex justify-center items-center'>Sports</li>
+                        <li className='hover:bg-sky-500 px-3 w-full text-bla flex  items-center'>Entertainment</li>
+                        <li className='hover:bg-sky-500 px-3 w-full flex items-center'>Technology</li>
+                        <li className='hover:bg-sky-500 px-3 w-full flex items-center'>Local</li>
+                        <li className='hover:bg-sky-500 px-3 w-full flex items-center'>World-Wide</li>
+                </ul> */}
+
         </nav>
 
 
-
-
-
-        //     <nav>
-        //   <div className="bg-slate-800 px-6 py-2 w-full h-12 flex items-center justify-center">
-        //     <a href="#" className="text-white font-semibold tracking-widest text-4xl">
-        //       <h1>BBC</h1>
-        //     </a>
-        //   </div>
-        // </nav>
 
     )
 }
